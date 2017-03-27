c9d8a1e32c00aefbf16f941533680823785cb2aa
siosio <siosio12@gmail.com>
2017-03-27 10:13:56 +0900

    BigDecimalのscaleをチェックするメソッドを追加

---
+++ src/main/java/nablarch/core/util/NumberUtil.java
@@ -0,0 +1,59 @@
+package nablarch.core.util;
+
+import java.math.BigDecimal;
+
+import nablarch.core.repository.SystemRepository;
+
+/**
+ * 数値に関するユーティリティクラス。
+ *
+ * @author siosio
+ */
+public final class NumberUtil {
+
+    /** BigDecimalのscale */
+    private static final int MAX_SCALE = 9999;
+
+    /**
+     * コンストラクタ。
+     */
+    private NumberUtil() {
+    }
+
+    /**
+     * {@link BigDecimal#scale()} が許容する範囲内かをチェックする。
+     * <p>
+     * 許容する範囲は、{@link #getMaxScale()}より取得する。
+     *
+     * @param value チェック対象
+     */
+    public static void verifyBigDecimalScale(final BigDecimal value) {
+        final int maxScale = getMaxScale();
+        final int scale = value.scale();
+        if (scale < -maxScale || scale > maxScale) {
+            throw new IllegalArgumentException(
+                    String.format("Illegal scale(%d): needs to be between(-%d, %d)", scale, maxScale, maxScale));
+        }
+    }
+
+    /**
+     * 許容するscaleを返す。
+     * <p>
+     * システムリポジトリから「nablarch.max_scale」が取得出来る場合はその値を、
+     * 取得できない場合はデフォルトの{@link #MAX_SCALE}を返す。
+     *
+     * @return 許容するscale
+     */
+    private static int getMaxScale() {
+        final Object maxScale = SystemRepository.get("nablarch.max_scale");
+        if (maxScale == null) {
+            return MAX_SCALE;
+        } else {
+            try {
+                return Integer.valueOf(maxScale.toString());
+            } catch (NumberFormatException ignored) {
+                return MAX_SCALE;
+            }
+        }
+    }
+}
---
+++ src/test/java/nablarch/core/util/NumberUtilTest.java
@@ -0,0 +1,127 @@
+package nablarch.core.util;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import nablarch.core.repository.ObjectLoader;
+import nablarch.core.repository.SystemRepository;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+/**
+ * {@link NumberUtil}のテスト。
+ */
+@RunWith(Enclosed.class)
+public class NumberUtilTest {
+
+    public static class VerifyBigDecimalScale {
+        
+        @Rule
+        public ExpectedException expectedException = ExpectedException.none();
+        
+        @Test
+        public void scaleが許容範囲の下限の場合例外は送出されないこと() {
+            final BigDecimal decimal = new BigDecimal("1e9999");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+        
+        @Test
+        public void 小数有りでscaleが許容範囲の下限の場合は例外は送出されないこと() {
+            final BigDecimal decimal = new BigDecimal("0.1e10000");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+        
+        @Test
+        public void scaleが許容範囲の上限の場合例外は送出されないこと() {
+            final BigDecimal decimal = new BigDecimal("1e-9999");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+        
+        @Test
+        public void 小数有りでscaleが許容範囲の上限の場合は例外が送出されないこと() {
+            final BigDecimal decimal = new BigDecimal("1.1e-9998");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+        
+        @Test
+        public void scaleが許容範囲の下限以下の場合例外が送出されること() {
+            final BigDecimal decimal = new BigDecimal("1e10000");
+
+            expectedException.expect(IllegalArgumentException.class);
+            expectedException.expectMessage("Illegal scale(-10000): needs to be between(-9999, 9999)");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+        
+        @Test
+        public void 小数有りでscaleが許容範囲の下限以下の場合は例外が送出されること() {
+            final BigDecimal decimal = new BigDecimal("0.1e10001");
+            
+            expectedException.expect(IllegalArgumentException.class);
+            expectedException.expectMessage("Illegal scale(-10000): needs to be between(-9999, 9999)");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+        
+        @Test
+        public void scaleが許容範囲の上限以上の場合は例外が送出されること() {
+            final BigDecimal decimal = new BigDecimal("1e-10000");
+            expectedException.expect(IllegalArgumentException.class);
+            expectedException.expectMessage("Illegal scale(10000): needs to be between(-9999, 9999)");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+        
+        @Test
+        public void 小数有りでscaleが許容範囲の上限以上の場合は例外が送出されないこと() {
+            final BigDecimal decimal = new BigDecimal("1.1e-10000");
+            expectedException.expect(IllegalArgumentException.class);
+            expectedException.expectMessage("Illegal scale(10001): needs to be between(-9999, 9999)");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+        
+        @Test
+        public void 指数表現ではない値の場合は例外が送出されないこと() throws Exception {
+            NumberUtil.verifyBigDecimalScale(new BigDecimal("1.11111111111111111111"));
+            NumberUtil.verifyBigDecimalScale(BigDecimal.valueOf(Long.MAX_VALUE));
+        }
+        
+        @Test
+        public void scaleの許容範囲を設定で変更出来ること() throws Exception {
+            SystemRepository.load(new ObjectLoader() {
+                @Override
+                public Map<String, Object> load() {
+                    final Map<String, Object> result = new HashMap<String, Object>();
+                    result.put("nablarch.max_scale", 10);
+                    return result;
+                }
+            });
+            
+            // scale->11
+            final BigDecimal decimal = new BigDecimal("1e-11");
+            expectedException.expect(IllegalArgumentException.class);
+            expectedException.expectMessage("Illegal scale(11): needs to be between(-10, 10)");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+        
+        @Test
+        public void scaleの許容範囲に不正な値を設定した場合デフォルトが使用されること() throws Exception {
+            SystemRepository.load(new ObjectLoader() {
+                @Override
+                public Map<String, Object> load() {
+                    final Map<String, Object> result = new HashMap<String, Object>();
+                    result.put("nablarch.max_scale", "10あ");
+                    return result;
+                }
+            });
+
+            // scale->11
+            final BigDecimal decimal = new BigDecimal("1e-100000");
+            expectedException.expect(IllegalArgumentException.class);
+            expectedException.expectMessage("Illegal scale(100000): needs to be between(-9999, 9999)");
+            NumberUtil.verifyBigDecimalScale(decimal);
+        }
+    }
+}
