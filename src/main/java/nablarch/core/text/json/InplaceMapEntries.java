2bdd1b1bbbd357541d62ff85b3ad42a9e8294df1
shuji-k <kitamura.shuji@tis.co.jp>
2021-03-09 16:58:54 +0900

    docs:InplaceMapEntriesクラスのJavadoc説明漏れ対応（author漏れのため再コミット）

--- src/main/java/nablarch/core/text/json/InplaceMapEntries.java
+++ src/main/java/nablarch/core/text/json/InplaceMapEntries.java
@@ -4,6 +4,7 @@ package nablarch.core.text.json;
  * Mapへの埋め込み用クラス。<br>
  * {@link MapToJsonSerializer}で組み立て済みのJSON構文となる文字列を埋め込むために使用する。<br>
  * 組み立て済みのJSON構文には、前後にオブジェクトの開始、終了マーカーや値のセパレーターは含まないこと。
+ * @author Shuji Kitamura
  */
 public class InplaceMapEntries {
 
