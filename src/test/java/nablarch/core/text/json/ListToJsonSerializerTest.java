3266c9c9457f4ad8444a6ab405d6a9deec12c219
shuji-k <kitamura.shuji@tis.co.jp>
2021-03-11 18:35:24 +0900

    test: ListのList、配列のList、MapのListのテストを追加

--- src/test/java/nablarch/core/text/json/ListToJsonSerializerTest.java
+++ src/test/java/nablarch/core/text/json/ListToJsonSerializerTest.java
@@ -8,7 +8,11 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.util.*;
 
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.isJson;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.withJsonPath;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.core.Is.is;
 
 /**
@@ -94,4 +98,50 @@ public class ListToJsonSerializerTest {
         assertThat(writer.toString(), is("[\"foo\",123,\"baz\"]"));
     }
 
+    @Test
+    public void ListのListがシリアライズできること() throws Exception {
+
+        List<List<Integer>> arrayValue = Arrays.asList(
+                Arrays.asList(0, 1, 2),
+                Arrays.asList(10, 11, 12)
+        );
+
+        serializer.serialize(writer, arrayValue);
+        assertThat(writer.toString(), is("[[0,1,2],[10,11,12]]"));
+    }
+
+    @Test
+    public void 配列のListがシリアライズできること() throws Exception {
+
+        List<Integer[]> arrayValue = Arrays.asList(
+                new Integer[] {0, 1, 2},
+                new Integer[] {10, 11, 12}
+        );
+
+        serializer.serialize(writer, arrayValue);
+        assertThat(writer.toString(), is("[[0,1,2],[10,11,12]]"));
+    }
+
+    @Test
+    public void MapのListがシリアライズできること() throws Exception {
+
+        List<Map<String, Integer>> arrayValue = new ArrayList<Map<String, Integer>>();
+        arrayValue.add(new HashMap<String, Integer>());
+        arrayValue.get(0).put("key0-0", 0);
+        arrayValue.get(0).put("key0-1", 1);
+        arrayValue.get(0).put("key0-2", 2);
+        arrayValue.add(new HashMap<String, Integer>());
+        arrayValue.get(1).put("key1-0", 10);
+        arrayValue.get(1).put("key1-1", 11);
+        arrayValue.get(1).put("key1-2", 12);
+
+        serializer.serialize(writer, arrayValue);
+        assertThat(writer.toString(), isJson(allOf(
+                withJsonPath("$[0]", hasEntry("key0-0", 0)),
+                withJsonPath("$[0]", hasEntry("key0-1", 1)),
+                withJsonPath("$[0]", hasEntry("key0-2", 2)),
+                withJsonPath("$[1]", hasEntry("key1-0", 10)),
+                withJsonPath("$[1]", hasEntry("key1-1", 11)),
+                withJsonPath("$[1]", hasEntry("key1-2", 12)))));
+    }
 }
