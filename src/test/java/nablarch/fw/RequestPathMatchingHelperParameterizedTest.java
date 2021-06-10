953653c6e2ee5fed74ce6e919fbda1c58a05c6b2
opengl-8080 <opengl-8080@users.noreply.github.com>
2021-06-10 17:47:42 +0900

    test: レビュー指摘対応でテストケースを追加(develop) (#42)

--- src/test/java/nablarch/fw/RequestPathMatchingHelperParameterizedTest.java
+++ src/test/java/nablarch/fw/RequestPathMatchingHelperParameterizedTest.java
@@ -88,7 +88,7 @@ public class RequestPathMatchingHelperParameterizedTest {
                 "//*.jsp    , /app/index.html     , false",
                 "//*.jsp    , /app/admin/index.jsp, true ",
 
-                /* ディレクトリパスが1つ以上の階層を持ち "//" で終わる */
+                /* ディレクトリパスが1つの階層を持ち "//" で終わる */
                 // リソース名が空（ディレクトリパスの前方一致判定のみが行われる）
                 "/app//     , /app                , false",
                 "/app//     , /app/               , true ",
@@ -123,7 +123,112 @@ public class RequestPathMatchingHelperParameterizedTest {
                 "/app//*.jsp, /zzz/app/index.jsp  , false",
                 "/app//*.jsp, /app/zzz/index.jsp  , true ",
 
-                /* ディレクトリパスが1つ以上の階層を持ち "/" で終わり、 "*" を使用している */
+                /* ディレクトリパスが1つの階層を持ち "/" で終わり、 "*" を使用している */
+                // リソース名が空
+                "/*/       , /                  , false",
+                "/*/       , /app               , false",
+                "/*/       , /app/              , true ",
+                "/*/       , /xyz/              , true ",
+                "/*/       , /app/xyz           , false",
+                "/*/       , /app/xyz/          , false",
+                // リソース名が "*" のみ
+                "/*/*      , /                  , false",
+                "/*/*      , /app               , false",
+                "/*/*      , /app/              , true ",
+                "/*/*      , /xyz/              , true ",
+                "/*/*      , /app/xyz           , true ",
+                "/*/*      , /xyz/app           , true ",
+                "/*/*      , /app/xyz/          , false",
+                "/*/*      , /app/xyz.jsp       , false",
+                "/*/*      , /app/xyz/zzz       , false",
+                // リソース名が "*" 以外のみ
+                "/*/test   , /                  , false",
+                "/*/test   , /app               , false",
+                "/*/test   , /app/              , false",
+                "/*/test   , /app/test          , true ",
+                "/*/test   , /xyz/test          , true ",
+                "/*/test   , /app/test/         , false",
+                "/*/test   , /app/test/test     , false",
+                "/*/test   , /app/test.jsp      , false",
+                // リソース名が混合
+                "/*/*.jsp  , /                  , false",
+                "/*/*.jsp  , /index.jsp         , false",
+                "/*/*.jsp  , /app               , false",
+                "/*/*.jsp  , /app/              , false",
+                "/*/*.jsp  , /app/index.jsp     , true ",
+                "/*/*.jsp  , /app/test.jsp      , true ",
+                "/*/*.jsp  , /app/index.html    , false",
+                "/*/*.jsp  , /xyz/test.jsp      , true ",
+                "/*/*.jsp  , /app/xyz/index.jsp , false",
+
+                /* ディレクトリパスが1つの階層を持ち "/" で終わり、 "*" を使用していない */
+                // リソース名が空
+                "/app/      , /app                    , false",
+                "/app/      , /app/                   , true ",
+                "/app/      , /xyz/                   , false",
+                "/app/      , /app/xyz                , false",
+                "/app/      , /xyz/app/               , false",
+                // リソース名が "*" のみ
+                "/app/*     , /app                    , false",
+                "/app/*     , /app/                   , true ",
+                "/app/*     , /xyz/                   , false",
+                "/app/*     , /app/xyz                , true ",
+                "/app/*     , /app/test               , true ",
+                "/app/*     , /app/xyz.jsp            , false",
+                "/app/*     , /app/xyz/               , false",
+                "/app/*     , /xyz/app/xyz            , false",
+                // リソース名が "*" 以外のみ
+                "/app/admin , /app                    , false",
+                "/app/admin , /app/admin              , true ",
+                "/app/admin , /app/xyz                , false",
+                "/app/admin , /app/admin/             , false",
+                "/app/admin , /app/admin.jsp          , false",
+                "/app/admin , /xyz/admin              , false",
+                "/app/admin , /xyz/app/admin          , false",
+                // リソース名が混合
+                "/app/*.jsp , /app                    , false",
+                "/app/*.jsp , /app/index.jsp          , true ",
+                "/app/*.jsp , /app/test.jsp           , true ",
+                "/app/*.jsp , /xyz/index.jsp          , false",
+                "/app/*.jsp , /app/xyz/index.jsp      , false",
+                "/app/*.jsp , /xyz/app/index.jsp      , false",
+
+                /* ディレクトリパスが2つの階層を持ち "//" で終わる */
+                // リソース名が空（ディレクトリパスの前方一致判定のみが行われる）
+                "/app/xyz//     , /app/xyz                , false",
+                "/app/xyz//     , /app/xyz/               , true ",
+                "/app/xyz//     , /app/xyz/admin/         , true ",
+                "/app/xyz//     , /app/xyz/admin/index.jsp, true ",
+                "/app/xyz//     , /abc/xyz/app/admin/     , false",
+                // リソース名が "*" のみ
+                "/app/xyz//*    , /app/xyz                , false",
+                "/app/xyz//*    , /app/xyz/               , true ",
+                "/app/xyz//*    , /app/xyz/admin          , true ",
+                "/app/xyz//*    , /app/xyz/admin/         , true ",
+                "/app/xyz//*    , /app/xyz/admin/index    , true ",
+                "/app/xyz//*    , /app/xyz/admin/index.jsp, false",
+                "/app/xyz//*    , /abc/xyz/app/admin/     , false",
+                // リソース名が "*" 以外のみ
+                "/app/xyz//admin, /app/xyz                , false",
+                "/app/xyz//admin, /app/xyz/               , false",
+                "/app/xyz//admin, /app/xyz/admin          , true ",
+                "/app/xyz//admin, /zzz/xyz/admin          , false",
+                "/app/xyz//admin, /zzz/xyz/app/admin      , false",
+                "/app/xyz//admin, /app/xyz/admin/         , false",
+                "/app/xyz//admin, /app/xyz/admin.jsp      , false",
+                "/app/xyz//admin, /app/xyz/zzz/admin      , true ",
+                // リソース名が混合
+                "/app/xyz//*.jsp, /app/xyz                , false",
+                "/app/xyz//*.jsp, /app/xyz.jsp            , false",
+                "/app/xyz//*.jsp, /app/xyz/               , false",
+                "/app/xyz//*.jsp, /app/xyz/index.jsp      , true ",
+                "/app/xyz//*.jsp, /app/xyz/test.jsp       , true ",
+                "/app/xyz//*.jsp, /app/xyz/index.html     , false",
+                "/app/xyz//*.jsp, /zzz/xyz/index.jsp      , false",
+                "/app/xyz//*.jsp, /zzz/xyz/app/index.jsp  , false",
+                "/app/xyz//*.jsp, /app/xyz/zzz/index.jsp  , true ",
+
+                /* ディレクトリパスが2つの階層を持ち "/" で終わり、 "*" を1つ使用している */
                 // リソース名が空
                 "/app/*/       , /app                   , false",
                 "/app/*/       , /app/                  , false",
@@ -168,7 +273,54 @@ public class RequestPathMatchingHelperParameterizedTest {
                 "/app/*/*.jsp  , /app/xxx/yyy/index.jsp , false",
                 "/app/*/*.jsp  , /xxx/app/xxx/index.jsp , false",
 
-                /* ディレクトリパスが1つ以上の階層を持ち "/" で終わり、 "*" を使用していない */
+                /* ディレクトリパスが2つの階層を持ち "/" で終わり、 "*" を2つ使用している */
+                // リソース名が空
+                "/*/*/       , /app                   , false",
+                "/*/*/       , /app/                  , false",
+                "/*/*/       , /app/xxx               , false",
+                "/*/*/       , /app/xxx/              , true ",
+                "/*/*/       , /xyz/abc/              , true ",
+                "/*/*/       , /app/xxx/yyy           , false",
+                "/*/*/       , /app/xxx/yyy/          , false",
+                // リソース名が "*" のみ
+                "/*/*/*      , /app                   , false",
+                "/*/*/*      , /app/                  , false",
+                "/*/*/*      , /app/xxx               , false",
+                "/*/*/*      , /app/xxx/              , true ",
+                "/*/*/*      , /xxx/app/              , true ",
+                "/*/*/*      , /app/xxx/yyy           , true ",
+                "/*/*/*      , /xxx/yyy/zzz           , true ",
+                "/*/*/*      , /app/xxx/yyy/          , false",
+                "/*/*/*      , /app/xxx/yyy.jsp       , false",
+                "/*/*/*      , /app/xxx/yyy/zzz       , false",
+                // リソース名が "*" 以外のみ
+                "/*/*/test   , /app                   , false",
+                "/*/*/test   , /test                  , false",
+                "/*/*/test   , /app/                  , false",
+                "/*/*/test   , /app/test              , false",
+                "/*/*/test   , /app/xxx               , false",
+                "/*/*/test   , /app/xxx/              , false",
+                "/*/*/test   , /app/xxx/test          , true ",
+                "/*/*/test   , /xxx/app/test          , true ",
+                "/*/*/test   , /app/xxx/test/         , false",
+                "/*/*/test   , /app/xxx/yyy           , false",
+                "/*/*/test   , /app/xxx/test.jsp      , false",
+                "/*/*/test   , /app/xxx/test/yyy      , false",
+                "/*/*/test   , /app/xxx/yyy/test      , false",
+                // リソース名が混合
+                "/*/*/*.jsp  , /app                   , false",
+                "/*/*/*.jsp  , /index.jsp             , false",
+                "/*/*/*.jsp  , /app/                  , false",
+                "/*/*/*.jsp  , /app/index.jsp         , false",
+                "/*/*/*.jsp  , /app/xxx               , false",
+                "/*/*/*.jsp  , /app/xxx/              , false",
+                "/*/*/*.jsp  , /app/xxx/index.jsp     , true ",
+                "/*/*/*.jsp  , /app/xxx/test.jsp      , true ",
+                "/*/*/*.jsp  , /xxx/app/index.jsp     , true ",
+                "/*/*/*.jsp  , /app/xxx/index.html    , false",
+                "/*/*/*.jsp  , /app/xxx/yyy/index.jsp , false",
+
+                /* ディレクトリパスが2つの階層を持ち "/" で終わり、 "*" を使用していない */
                 // リソース名が空
                 "/app/xyz/      , /app                    , false",
                 "/app/xyz/      , /app/                   , false",
