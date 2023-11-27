81c2f9442a598f6653807fccf06eefa2eb0814fe
kankichi <ichikawa.kankichi@tis.co.jp>
2023-11-27 10:49:43 +0900

    test: レビュー指摘の修正とInspection対応

--- src/test/java/nablarch/core/date/SystemTimeUtilTest.java
+++ src/test/java/nablarch/core/date/SystemTimeUtilTest.java
@@ -1,39 +1,33 @@
 package nablarch.core.date;
 
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThrows;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.TimeZone;
 
 import nablarch.core.ThreadContext;
-import nablarch.core.repository.ObjectLoader;
 import nablarch.core.repository.SystemRepository;
 import nablarch.util.FixedSystemTimeProvider;
 
 import org.junit.After;
-import org.junit.Assert;
+import org.hamcrest.MatcherAssert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 /**
  * {@link SystemTimeUtil}のテストクラス
  * @author Miki Habu
  */
 public class SystemTimeUtilTest {
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+    
     private TimeZone defaultTimeZone;
 
     /**
@@ -44,15 +38,12 @@ public class SystemTimeUtilTest {
     public void setUp() {
         defaultTimeZone = TimeZone.getDefault();
         // リポジトリの初期化
-        SystemRepository.load(new ObjectLoader() {
-            @Override
-            public Map<String, Object> load() {
-                HashMap<String, Object> result = new HashMap<String, Object>();
-                FixedSystemTimeProvider provider = new FixedSystemTimeProvider();
-                provider.setFixedDate("20110107123456");
-                result.put("systemTimeProvider", provider);
-                return result;
-            }
+        SystemRepository.load(() -> {
+            HashMap<String, Object> result = new HashMap<>();
+            FixedSystemTimeProvider provider = new FixedSystemTimeProvider();
+            provider.setFixedDate("20110107123456");
+            result.put("systemTimeProvider", provider);
+            return result;
         });
     }
 
@@ -74,22 +65,19 @@ public class SystemTimeUtilTest {
     public void testGetDate() throws Exception {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
         Date expected = sdf.parse("20110107123456000");
-        Assert.assertThat(SystemTimeUtil.getDate(), is(expected));
+        MatcherAssert.assertThat(SystemTimeUtil.getDate(), is(expected));
     }
 
     /**
      * {@link SystemTimeUtil#getDate()} のテスト。
      * <p/>
      * リポジトリに値がない場合、例外を送出するかどうか。
-     * @throws Exception
      */
     @Test
-    public void testGetDateErr() throws Exception {
+    public void testGetDateErr() {
         SystemRepository.clear();
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("specified systemTimeProvider is not registered in SystemRepository.");
-
-        SystemTimeUtil.getDate();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, SystemTimeUtil::getDate);
+        MatcherAssert.assertThat(exception.getMessage(), is("specified systemTimeProvider is not registered in SystemRepository."));
     }
     
     /**
@@ -98,13 +86,13 @@ public class SystemTimeUtilTest {
     @Test
     public void testGetTimestamp() {
         Timestamp expected = Timestamp.valueOf("2011-01-07 12:34:56.000000000");
-        Assert.assertThat(SystemTimeUtil.getTimestamp(), is(expected));
+        MatcherAssert.assertThat(SystemTimeUtil.getTimestamp(), is(expected));
     }
 
     /**
      * {@link SystemTimeUtil#getLocalDateTime()}のテスト
      * <p/>
-     * スレッドコンテキストのTimeZoneがnullの場合、システムデフォルトのタイムゾーンを取得するかどうか。
+     * スレッドコンテキストにタイムゾーンが設定されていない場合、システムデフォルトのタイムゾーンを使用して{@link LocalDateTime}を取得できること。
      */
     @Test
     public void testGetLocalDateTimeDefault() {
@@ -114,39 +102,28 @@ public class SystemTimeUtilTest {
         LocalDateTime expected = ZonedDateTime
             .parse(
                 "2011-01-07T12:34:56",
-                DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.of(String.valueOf(defaultTimeZone.toZoneId())))
+                DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(defaultTimeZone.toZoneId())
             )// 文字列を、システムデフォルトのタイムゾーンの日時と考えるようにしたうえで変換
             .withZoneSameInstant(ZoneId.of("America/Los_Angeles"))// タイムゾーンをアメリカに変更
             .toLocalDateTime();
 
-        Assert.assertNull(ThreadContext.getTimeZone());
-        Assert.assertEquals(expected, SystemTimeUtil.getLocalDateTime());
+        MatcherAssert.assertThat(SystemTimeUtil.getLocalDateTime(), is(expected));
     }
 
     /**
      * {@link SystemTimeUtil#getLocalDateTime()} のテスト。
      * <p/>
-     * スレッドコンテキストのTimeZoneがnullではない場合、システムデフォルトのタイムゾーンを取得しないかどうか。
+     * スレッドコンテキストにタイムゾーンが設定されている場合、スレッドコンテキストのタイムゾーンを使用する。
      */
     @Test
     public void testGetLocalDateTime() {
-        ThreadContext.setTimeZone(TimeZone.getTimeZone(String.valueOf(defaultTimeZone.toZoneId())));
+        ThreadContext.setTimeZone(defaultTimeZone);
         TimeZone.setDefault(TimeZone.getTimeZone("America/Los_Angeles"));
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuuMMddHHmmss");
         LocalDateTime expected = LocalDateTime.parse("20110107123456", dtf);
 
-        Assert.assertEquals(expected, SystemTimeUtil.getLocalDateTime());
-    }
-
-    /**
-     * {@link SystemTimeUtil#getLocalDate()}のテスト
-     */
-    @Test
-    public void testGetLocalDate() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuuMMdd");
-        LocalDate expected = LocalDate.parse("20110107", dtf);
-        Assert.assertEquals(expected, SystemTimeUtil.getLocalDate());
+        MatcherAssert.assertThat(SystemTimeUtil.getLocalDateTime(), is(expected));
     }
 
     /**
@@ -154,7 +131,7 @@ public class SystemTimeUtilTest {
      */
     @Test
     public void testGetCurrentDateString() {
-        Assert.assertThat(SystemTimeUtil.getDateString(), is("20110107"));
+        MatcherAssert.assertThat(SystemTimeUtil.getDateString(), is("20110107"));
     }
 
     /**
@@ -162,7 +139,7 @@ public class SystemTimeUtilTest {
      */
     @Test
     public void testGetDateTimeString() {
-       Assert.assertThat(SystemTimeUtil.getDateTimeString(), is("20110107123456")); 
+       MatcherAssert.assertThat(SystemTimeUtil.getDateTimeString(), is("20110107123456")); 
     }
     
     /**
@@ -170,7 +147,7 @@ public class SystemTimeUtilTest {
      */
     @Test
     public void testGetDateTimeMillisString() {
-        Assert.assertThat(SystemTimeUtil.getDateTimeMillisString(), is("20110107123456000"));         
+        MatcherAssert.assertThat(SystemTimeUtil.getDateTimeMillisString(), is("20110107123456000"));         
     }
     
 }
